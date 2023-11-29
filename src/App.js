259aa2227961de69d08c51865dfc7e3c922fe511
Eduardo <educcarnn@gmail.com>
2023-11-29 18:31:39 -0300

    fix: version to react 17.02

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js
+
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/header";
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
